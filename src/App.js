1227c55e96aed32d64581555845f8b8506c0e1f2
compumike08 <compumike08@users.noreply.github.com>
2017-10-08 23:31:33 -0400

    Move AppBar to new Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import AppBar from 'material-ui/AppBar';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AppBar title="GitHub Status App" />
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const Header = () => {
+  return (
+    <AppBar title="GitHub Status App" />
+  );
+};
+
+export default Header;
