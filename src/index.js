87688c2f61dcd7a5a03e80fbfd9496d199d06988
compumike08 <compumike08@users.noreply.github.com>
2017-10-08 23:16:45 -0400

    Add Material-UI MuiThemeProvider componenet

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppWrapper = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(<AppWrapper />, document.getElementById('root'));
 registerServiceWorker();
