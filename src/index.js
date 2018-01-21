924bb95d5509b123c876e689cac44a30784aed84
compumike08 <compumike08@users.noreply.github.com>
2018-01-21 16:35:42 -0500

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import configureStore from './store/configureStore';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = configureStore();
+
 const AppWrapper = () => (
-  <MuiThemeProvider>
-    <App />
-  </MuiThemeProvider>
+  <Provider store={store}>
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
+  </Provider>
 );
 
-ReactDOM.render(<AppWrapper />, document.getElementById('root'));
+render(
+  <AppWrapper />,
+  document.getElementById('root')
+);
 registerServiceWorker();
